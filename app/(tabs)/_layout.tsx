d09d961f89a722481e841d5515b484fd738591dc
markzwehtet <markzwehtet@gmail.com>
2025-08-22 01:29:26 -0400

    Implementing databases

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Redirect, Slot } from 'expo-router'
 import { useAuthStore } from '@/store/auth.store';
+import { TabBarIconProps } from '@/type';
+import { Ionicons } from '@expo/vector-icons';
+import { Redirect, Tabs } from 'expo-router';
+import React from 'react';
+import { Text, View } from 'react-native';
 
+export const TabBarIcon = ({ focused, icon, title}: TabBarIconProps) => {
+    return (
+        <View className="tab-icon">
+            <Ionicons
+                name={icon}
+                size={24}
+                color={focused ? '#FF6B35' : 'grey'} // Use actual colors
+                />
+            <Text className={`text-sm font-bold ${focused ? 'text-primary' : 'grey'}`}>{title}</Text>
+        </View>
+    );
+}
 export default function TabLayout() {
 
 
@@ -15,6 +29,82 @@ export default function TabLayout() {
         );
     }
   return (
-   <Slot />
+    <Tabs
+    screenOptions={{
+        headerShown: false, 
+        tabBarShowLabel: false,
+        tabBarStyle: {
+        borderTopLeftRadius: 50, 
+        borderTopRightRadius: 50, 
+        borderBottomLeftRadius: 50, 
+        borderBottomRightRadius: 50,
+        marginHorizontal: 20,
+         height: 80, 
+         position:'absolute',
+        bottom: 40,
+         backgroundColor: 'white', 
+         shadowColor: 'black',
+         shadowOffset: { width: 0, height: 6 }, 
+         shadowOpacity: 0.1,
+         shadowRadius: 4,
+         elevation: 5 
+    }
+}}
+    >
+
+        <Tabs.Screen
+            name="index"
+            options={{
+                title: 'Home',
+                tabBarIcon: ({ focused }) => (
+                    <TabBarIcon
+                        focused={focused}
+                        icon="home-outline"
+                        title="Home"
+                    />
+                ),
+            }}
+        />
+         <Tabs.Screen
+            name="search"
+            options={{
+                title: 'Search',
+                tabBarIcon: ({ focused }) => (
+                    <TabBarIcon
+                        focused={focused}
+                        icon="search-outline"
+                        title="Search"
+                    />
+                ),
+            }}
+        />
+         <Tabs.Screen
+            name="cart"
+            options={{
+                title: 'Cart',
+                tabBarIcon: ({ focused }) => (
+                    <TabBarIcon
+                        focused={focused}
+                        icon="cart-outline"
+                        title="Cart"
+                    />
+                ),
+            }}
+        />
+         <Tabs.Screen
+            name="profile"
+            options={{
+                title: 'Profile',
+                tabBarIcon: ({ focused }) => (
+                    <TabBarIcon
+                        focused={focused}
+                        icon="person-outline"
+                        title="Profile"
+                    />
+                ),
+            }}
+        />
+   
+    </Tabs>
   )
 }
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -6,7 +6,12 @@ export const appwriteConfig = {
   platform: "com.markzwe.food_ordering",
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID || "",
   databaseId: '68a4c35b00051ca1c318',
+  bucketId: '68a7fe5a00280b2b4f38',
   userCollectionId: '68a528cb0022006b42c3',
+  categoriesCollectionId: '68a7f8200033cc4de76a',
+  menuCollectionId: '68a7f8c2001d263d0047',
+  customizationsCollectionId: '68a7f9b8001d4dd82948', 
+  menuCustomizationsCollectionId: '68a7fc3e0029d2724d11'
 };
 
 export const client = new Client();
@@ -17,21 +22,21 @@ client.setPlatform(appwriteConfig.platform);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
- const avatars = new Avatars(client);
+const avatars = new Avatars(client);
 
 export const createUser = async ({ name, email, password }: CreateUserParams) => {
   try {
     const newAccount = await account.create(ID.unique(), email, password, name);
 
-    if (!newAccount) { throw Error("Failed to create user account")}
+    if (!newAccount) { throw Error("Failed to create user account") }
 
     //auto sign in the user after creation
-    await signIn({email, password});
+    await signIn({ email, password });
 
-    const avatarUrl = avatars.getInitialsURL(name);
+    const avatarUrl = `https://ui-avatars.com/api/?name=${encodeURIComponent(name)}&background=random&color=fff&size=150`;
 
     const newUser = await databases.createDocument(
-      appwriteConfig.databaseId, 
+      appwriteConfig.databaseId,
       appwriteConfig.userCollectionId,
       ID.unique(),
       {
@@ -47,18 +52,18 @@ export const createUser = async ({ name, email, password }: CreateUserParams) =>
     throw new Error("Failed to create user: " + (error instanceof Error ? error.message : String(error)));
   }
 }
- export const signIn = async ({email, password}: SignInParams) => {
+export const signIn = async ({ email, password }: SignInParams) => {
   try {
     const session = await account.createEmailPasswordSession(email, password);
   } catch (error) {
     throw new Error(error as string)
   }
- }
+}
 
- export const getCurrentUser = async () => {
+export const getCurrentUser = async () => {
   try {
     const currentAccount = await account.get();
-    if (!currentAccount) { throw Error("No user found")}
+    if (!currentAccount) { throw Error("No user found") }
 
     const currentUser = await databases.listDocuments(
       appwriteConfig.databaseId,
@@ -69,4 +74,4 @@ export const createUser = async ({ name, email, password }: CreateUserParams) =>
   } catch (error) {
     throw new Error(error as string)
   }
- }
+}
